ecb7cf493c522790150b4f2f27449e2f2cdb6b52
Nik Rowe <nikrowedev@gmail.com>
2020-11-22 04:49:38 -0800

    Recipes State Hook & Component import

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { firestore } from "./firebase";
 import "./App.css";
 
-function App() {
+import Recipes from "./components/Recipes";
+
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const snapshot = await firestore.collection("recipes").get();
 
-      snapshot.forEach((doc) => {
-        const id = doc.id;
-        const data = doc.data();
-
-        console.log("id, data :", { id, data });
+      const recipes = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
       });
+      console.log("recipes :", recipes);
+
+      setRecipes(recipes);
     };
 
     fetchData();
-  });
+  }, []);
+
+  const handleCreate = (recipe) => {
+    const { recipes } = this.useState;
+    setRecipes({ recipes: [recipe, ...recipes] });
+  };
 
   return (
     <div className="App">
       <h1>Rowe Cook Book</h1>
+      <Recipes recipes={recipes} onCreate={handleCreate} />
     </div>
   );
-}
+};
 
 export default App;
