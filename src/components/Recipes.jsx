8f47ab709b6012513e0e2fb566abbd3ddf1dff92
Nik Rowe <nikrowedev@gmail.com>
2020-11-22 20:23:54 -0800

    AddRecipe Component Import

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Recipe from "./Recipe";
+import AddRecipe from "./AddRecipe";
 
 const Recipes = ({ recipes, onCreate }) => {
-  //   console.log("recipes :", recipes);
+  console.log("recipes :", recipes);
   return (
     <section className="Recipes">
+      <AddRecipe onCreate={onCreate} />
       {recipes.map((recipe) => (
         <Recipe {...recipe} key={recipe.id} />
       ))}
