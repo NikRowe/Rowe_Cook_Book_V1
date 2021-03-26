ca9e095deda70aaf37740543e62d431bee706a28
Nik Rowe <nikrowedev@gmail.com>
2021-03-26 13:30:39 -0700

    Remove Recipe State from App into Firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { firestore, auth, createUserProfileDocument } from "./firebase";
+import { auth, createUserProfileDocument } from "./firebase";
 import "./App.css";
 
 import Recipes from "./components/Recipes";
-import { collectIdsAndDocs } from "./utilities";
 import Authentication from "./components/Authentication";
 
 const App = () => {
-  const [recipes, setRecipes] = useState([]);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    let unsubscribeFromFireStore = null;
     let unsubscribeFromAuth = null;
 
     /* gets firebase store data and subscribes via onSnapshot() then sets to state for visualization - this will use more api calls as it is calling the API and updating the app anytime this FB collection is being changed. 
@@ -19,16 +16,8 @@ const App = () => {
     You can totally use .get() instead of .onSnapshot() and manually setState() so that the api is only called upon refreshing the page to reduce our API calls. 
     */
     const fetchData = async () => {
-      unsubscribeFromFireStore = firestore
-        .collection("recipes")
-        .onSnapshot((snapshot) => {
-          const recipes = snapshot.docs.map(collectIdsAndDocs);
-          setRecipes(recipes);
-        });
-
       unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
         const user = await createUserProfileDocument(userAuth);
-        console.log("user:", user);
         setUser(user);
       });
     };
@@ -37,7 +26,6 @@ const App = () => {
 
     // "clean up" previously done with componentWillUnmount() to prevent memory leak //
     return () => {
-      unsubscribeFromFireStore();
       unsubscribeFromAuth();
     };
   }, []);
@@ -46,7 +34,7 @@ const App = () => {
     <div className="App">
       <h1>Rowe Cook Book</h1>
       <Authentication user={user} />
-      <Recipes recipes={recipes} />
+      <Recipes />
     </div>
   );
 };
