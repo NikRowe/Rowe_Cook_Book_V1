fbafad1836fba8a4ac0cfd6bb78a406cd4c6fb19
Nik Rowe <nikrowedev@gmail.com>
2021-03-31 12:34:05 -0700

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./App.css";
 
 import Recipes from "./components/Recipes";
 import Authentication from "./components/Authentication";
+import UserProfile from "./components/UserProfile";
+
+import { Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Rowe Cook Book</h1>
+      <Link to="/">
+        <h1>Rowe Cook Book</h1>
+      </Link>
       <Authentication />
-      <Recipes />
+      <Switch>
+        <Route exact path="/" component={Recipes} />
+        <Route exact path="/profile" component={UserProfile} />
+      </Switch>
     </div>
   );
 };
