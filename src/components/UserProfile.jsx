6f0e246b77ea6fc51090ff67187b195e51782e56
Nik Rowe <nikrowedev@gmail.com>
2021-04-01 11:02:09 -0700

    DisplayName update on page refresh

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, firestore } from "../firebase";
 
 const UserProfile = () => {
-  return <div>This is totally a User Profile</div>;
+  const [displayName, setDisplayName] = useState("");
+  let imageInput = null;
+
+  // const { uid } = auth.currentUser;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    name === "displayName" ? setDisplayName(value) : setDisplayName("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { uid } = auth.currentUser;
+
+    const userRef = firestore.doc(`users/${uid}`);
+
+    if (displayName) {
+      userRef.update({ displayName });
+    }
+  };
+
+  return (
+    <section className="UserProfile">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={displayName}
+          name="displayName"
+          onChange={handleChange}
+          placeholder="Display Name"
+        />
+        <input type="file" ref={(ref) => (imageInput = ref)} />
+        <input className="update" type="submit" />
+      </form>
+    </section>
+  );
 };
 
 export default UserProfile;
