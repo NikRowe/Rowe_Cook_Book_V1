3a758ba10c7c2962d75f7252f49bccb83559dbb2
Nik Rowe <nikrowedev@gmail.com>
2021-04-06 14:05:16 -0700

    Comments Component to Recipe Page

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -1,15 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Recipe from "./Recipe";
-// import Comments from "./Comments";
+import Comments from "./Comments";
 import { firestore } from "../firebase";
 import { collectIdsAndDocs } from "../utilities";
 
-const RecipePage = () => {
+import { withRouter } from "react-router-dom";
+
+const RecipePage = (props) => {
   const [recipe, setRecipe] = useState(null);
   const [comments, setComments] = useState([]);
 
-  return <div> Post Page ! </div>;
+  let recipeId = props.match.params.id;
+  let recipeRef = firestore.doc(`recipes/${recipeId}`);
+  let commentsRef = recipeRef.collection("comments");
+
+  useEffect(() => {
+    let unsubscribeFromRecipe = null;
+    let unsubscribeFromComments = null;
+
+    const fetchData = async () => {
+      unsubscribeFromRecipe = recipeRef.onSnapshot((snapshot) => {
+        const recipe = collectIdsAndDocs(snapshot);
+        setRecipe(recipe);
+      });
+
+      unsubscribeFromComments = commentsRef.onSnapshot((snapshot) => {
+        const comments = snapshot.docs.map(collectIdsAndDocs);
+        setComments(comments);
+      });
+    };
+
+    fetchData();
+
+    return () => {
+      unsubscribeFromRecipe();
+      unsubscribeFromComments();
+    };
+  });
+
+  return (
+    <section>
+      {recipe && <Recipe {...recipe} />}
+      <Comments comments={comments} onCreate={() => {}} />
+    </section>
+  );
 };
 
-export default RecipePage;
+export default withRouter(RecipePage);
