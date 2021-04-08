90e6a80f56ba4b32003c63b48a08fa8567d7285a
Nik Rowe <nikrowedev@gmail.com>
2021-04-08 14:13:20 -0700

    moment time stamp

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-// import moment from "moment";
+import moment from "moment";
 
 const Comment = ({ content, user, createdAt }) => {
   return (
     <article className="Comment">
       <span className="Comment--author">{user.displayName}</span>
       <span className="Comment--content">{content}</span>
-      {/* <span className="Comment--timestamp">{moment(createdAt).calendar()}</span> */}
+      <span className="Comment--timestamp">{moment(createdAt).calendar()}</span>
     </article>
   );
 };
